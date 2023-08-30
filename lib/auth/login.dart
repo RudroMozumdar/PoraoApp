6bbf8c6726c32f46164dec9df8952e3ef7fab5a1
RudroMozumdar <rudromozumdar890@gmail.com>
2023-08-31 01:17:44 +0600

    Ajaira Counter page removed

--- lib/auth/login.dart
+++ lib/auth/login.dart
@@ -71,115 +71,113 @@ class _LoginState extends State<Login> with SingleTickerProviderStateMixin {
 
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Container(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              AnimatedBuilder(
-                animation: _animacaoBlur!,
-                builder: (context, widget) {
-                  return Container(
-                    height: 400,
-                    decoration: const BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage("images/fundo.png"),
-                        fit: BoxFit.fill,
-                      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            AnimatedBuilder(
+              animation: _animacaoBlur!,
+              builder: (context, widget) {
+                return Container(
+                  height: 400,
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage("images/fundo.png"),
+                      fit: BoxFit.fill,
                     ),
-                    child: BackdropFilter(
-                      filter: ImageFilter.blur(
-                        sigmaX: _animacaoBlur!.value,
-                        sigmaY: _animacaoBlur!.value,
-                      ),
-                      child: Stack(
-                        children: [
-                          Positioned(
-                            left: 10,
-                            child: FadeTransition(
-                              opacity: _animacaoFade!,
-                              child: Image.asset("images/detalhe1.png"),
-                            ),
-                          ),
-                          Positioned(
-                            left: 50,
-                            child: FadeTransition(
-                              opacity: _animacaoFade!,
-                              child: Image.asset("images/detalhe2.png"),
-                            ),
-                          ),
-                        ],
-                      ),
+                  ),
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(
+                      sigmaX: _animacaoBlur!.value,
+                      sigmaY: _animacaoBlur!.value,
                     ),
-                  );
-                },
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 40, right: 40, top: 20),
-                child: Column(
-                  children: [
-                    AnimatedBuilder(
-                      animation: _animacaoSize!,
-                      builder: (context, widget) {
-                        return Container(
-                          width: _animacaoSize?.value,
-                          padding: const EdgeInsets.all(8),
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(20),
-                            boxShadow: const [
-                              BoxShadow(
-                                color: Colors.grey,
-                                blurRadius: 80,
-                                spreadRadius: 1,
-                              )
-                            ],
+                    child: Stack(
+                      children: [
+                        Positioned(
+                          left: 10,
+                          child: FadeTransition(
+                            opacity: _animacaoFade!,
+                            child: Image.asset("images/detalhe1.png"),
                           ),
-                          child: Column(
-                            children: [
-                              const InputCustomizado(
-                                hint: 'e-mail',
-                                obscure: false,
-                                icon: Icon(Icons.person),
-                              ),
-                              Container(
-                                decoration: const BoxDecoration(
-                                  boxShadow: [
-                                    BoxShadow(
-                                      color: Colors.grey,
-                                      spreadRadius: 0.5,
-                                      blurRadius: 0.5,
-                                    ),
-                                  ],
-                                ),
-                              ),
-                              const InputCustomizado(
-                                hint: 'senha',
-                                obscure: true,
-                                icon: Icon(Icons.lock),
-                              ),
-                            ],
-                          ),
-                        );
-                      },
-                    ),
-                    const SizedBox(height: 20),
-                    BotaoAnimado(controller: _controller!),
-                    const SizedBox(height: 10),
-                    FadeTransition(
-                      opacity: _animacaoFade!,
-                      child: const Text(
-                        "Esqueci minha senha",
-                        style: TextStyle(
-                          color: Color.fromRGBO(255, 100, 127, 1),
-                          fontWeight: FontWeight.bold,
                         ),
+                        Positioned(
+                          left: 50,
+                          child: FadeTransition(
+                            opacity: _animacaoFade!,
+                            child: Image.asset("images/detalhe2.png"),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 40, right: 40, top: 20),
+              child: Column(
+                children: [
+                  AnimatedBuilder(
+                    animation: _animacaoSize!,
+                    builder: (context, widget) {
+                      return Container(
+                        width: _animacaoSize?.value,
+                        padding: const EdgeInsets.all(8),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(20),
+                          boxShadow: const [
+                            BoxShadow(
+                              color: Colors.grey,
+                              blurRadius: 80,
+                              spreadRadius: 1,
+                            )
+                          ],
+                        ),
+                        child: Column(
+                          children: [
+                            const InputCustomizado(
+                              hint: 'e-mail',
+                              obscure: false,
+                              icon: Icon(Icons.person),
+                            ),
+                            Container(
+                              decoration: const BoxDecoration(
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.grey,
+                                    spreadRadius: 0.5,
+                                    blurRadius: 0.5,
+                                  ),
+                                ],
+                              ),
+                            ),
+                            const InputCustomizado(
+                              hint: 'senha',
+                              obscure: true,
+                              icon: Icon(Icons.lock),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                  const SizedBox(height: 20),
+                  BotaoAnimado(controller: _controller!),
+                  const SizedBox(height: 10),
+                  FadeTransition(
+                    opacity: _animacaoFade!,
+                    child: const Text(
+                      "Esqueci minha senha",
+                      style: TextStyle(
+                        color: Color.fromRGBO(255, 100, 127, 1),
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -67,30 +67,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              'hello world',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
+    return const Login();
   }
 }
